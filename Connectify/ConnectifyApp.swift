1aea4d2640b620ca6e8538a78a4c45d1c3ed3aa3
Louis Peitz <peitzlouis3@gmail.com>
2024-06-11 20:25:51 +0200

    Initial Commit

---
+++ Connectify/ConnectifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectifyApp.swift
+//  Connectify
+//
+//  Created by Louis Peitz on 11.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
